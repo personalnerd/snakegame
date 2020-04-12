5765b9b4ec16ea23b9be6eb51528432a16aaf940
Tarcisio Cavalcante <cisoxp@gmail.com>
2020-04-12 18:31:12 -0300

    Definir direçoes e impedir que a cobrinha suma do canvas

--- script.js
+++ script.js
@@ -21,7 +21,21 @@ function criarCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
