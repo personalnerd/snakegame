890f9034558ac60b08ab3642dda120a0d495212e
Tarcisio Cavalcante <cisoxp@gmail.com>
2020-04-12 18:43:38 -0300

    Definir aleatoriedade da comida e crescimento da cobrinha ao comer

--- script.js
+++ script.js
@@ -57,7 +57,12 @@ function iniciarJogo() {
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
