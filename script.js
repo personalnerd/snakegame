c9402bd31e84bf65c75e6ae0d7b64c0e61399c5a
Tarcisio Cavalcante <cisoxp@gmail.com>
2020-04-12 18:51:47 -0300

    Definir fim do jogo

--- script.js
+++ script.js
@@ -40,10 +40,20 @@ function update(event){
 }
 
 function iniciarJogo() {
+
+    // direções
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    //se a posição 0 (cabeça) se chocar com o corpo, ela para o jogo
+    for (i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
