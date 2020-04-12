1bb32ad6b14b8310858662c9d1aa2cdb846b673e
Tarcisio Cavalcante <cisoxp@gmail.com>
2020-04-12 18:11:39 -0300

    Criada a cobrinha

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d');
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
     context.fillStyle = 'lightgreen';
@@ -8,4 +13,12 @@ function criarBG() {
     // posição de x e y, altura e largura (16 quadradinhos * altura do box (32))
 }
 
+function criarCobrinha() {
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = 'green';
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
 criarBG();
+criarCobrinha();
